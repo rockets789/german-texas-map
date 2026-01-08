3e9c9c0d0246286f61161bebbb610ca5b618d474
rockets789 <ritvikrajesh107@gmail.com>
2026-01-07 18:22:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -220,10 +220,12 @@ with st.spinner('Filtering data and redrawing map...'):
         # --- END OF LOOP ---
 
     # 4. DRAW THE MARKER
-    folium.Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=folium.Popup(popup_html, max_width=350),
-        icon=folium.Icon(color=color, icon="info-sign")
-    ).add_to(marker_cluster)
+        folium.Marker(
+            location=[row['latitude'], row['longitude']],
+            popup=folium.Popup(popup_html, max_width=350),
+            icon=folium.Icon(color=color, icon="info-sign")
+            ).add_to(marker_cluster)
+except Exception:
+    continue
     # Display Map
     st_folium(m, width=None, height=600, returned_objects=[])
